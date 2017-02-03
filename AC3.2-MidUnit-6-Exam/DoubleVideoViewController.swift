62f7e50102848feb4bf47e2069dab5aa0301cdc3
Tom Seymour <tomeseymour44@gmail.com>
2017-02-03 15:01:57 -0500

    double video mvp

--- AC3.2-MidUnit-6-Exam/DoubleVideoViewController.swift
+++ AC3.2-MidUnit-6-Exam/DoubleVideoViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+import MobileCoreServices
 
-class DoubleVideoViewController: UIViewController, CellTitled {
+
+class DoubleVideoViewController: UIViewController, CellTitled, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var titleForCell: String = "Double Video"
 
     @IBOutlet weak var videoContainerTop: UIView!
     @IBOutlet weak var videoContainerBottom: UIView!
+    
+    var videoURL: URL?
+    
+    var topPlayer: AVPlayer?
+    var bottomPlayer: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +31,55 @@ class DoubleVideoViewController: UIViewController, CellTitled {
     }
 
     override func viewDidLayoutSubviews() {
+        
         // update your layers' frames here
     }
     
-    @IBAction func loadVideo(_ sender: UIButton) {
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if info[UIImagePickerControllerMediaType] as! String == String(kUTTypeMovie) {
+            if let url = info[UIImagePickerControllerReferenceURL] as? URL {
+                self.videoURL = url
+            }
+        }
+        self.dismiss(animated: true, completion: nil)
+        
+        if let url = self.videoURL {
+            let playerItem: AVPlayerItem = AVPlayerItem(url: url)
+            let player = AVPlayer(playerItem: playerItem)
+            let playerLayer = AVPlayerLayer(player: player)
+            
+            if self.topPlayer == nil || self.topPlayer?.rate == 0.0 {
+                videoContainerTop.layer.sublayers = nil
+                self.topPlayer = player
+                playerLayer.frame = self.videoContainerTop.bounds
+                videoContainerTop.layer.addSublayer(playerLayer)
+                self.topPlayer?.play()
+            }
+            else if self.bottomPlayer == nil || self.bottomPlayer?.rate == 0.0 {
+                videoContainerBottom.layer.sublayers = nil
+                self.bottomPlayer = player
+                playerLayer.frame = self.videoContainerBottom.bounds
+                videoContainerBottom.layer.addSublayer(playerLayer)
+                self.bottomPlayer?.play()                
+            }
+            else {
+                let alert = UIAlertController(title: "No Free Video Slots Availible", message: "Please wait until one of the videos on screen has finished before selecting another.", preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alert.addAction(okayAction)
+                present(alert, animated: true, completion: nil)
+            }
+            self.videoURL = nil
+        }
     }
+    
+    @IBAction func loadVideo(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = [String(kUTTypeMovie), String(kUTTypeImage)]
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
 }
